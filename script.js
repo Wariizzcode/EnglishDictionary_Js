b7543f439179d52edf9447e66ac351727e33d8f0
Abdulwariz001 <warizazeez05@gmail.com>
2025-09-16 23:59:09 +0100

    reuploading my dictionary project making some changes on it

--- script.js
+++ script.js
@@ -20,25 +20,23 @@ function empty_example(empty){
 }
 
 
-    let input = document.getElementById("input")
+    let inputValue = document.getElementById("input")
     let show = document.getElementById("show")
 
    function submit(){
- fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+input.value)
+ fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+inputValue.value)
  .then(res => res.json())
  .then(data =>{
     let results = "";
     for(let i = 0 ; i < data[0].meanings.length ; i++){
         results += `
                 <div class="container-item m-3">
-                    <b>
-                        <i class="fas fa-volume"></i>
                         <audio controls autoplay><source src="${data[0].phonetics[0].audio}"></audio>
-                    </b> <br><br>
-                    <h5>Transcription:<span>${data[0].phonetics[1].text}</span></h5>
+                    
+                    <h5>Transcription:<span>${data[0].phonetics[0].text}</span></h5>
                     <header>Part of speech:<span>${data[0].meanings[i].partOfSpeech}</span></header><br>
                     <h5>Meaning:<span>${data[0].meanings[i].definitions[0].definition}</span></h5>
-                    <p>Example:<span> ${empty_example(data[0].meanings[i].definitions[0].example)}</span></p> <hr>
+                    <p>Example:<span> ${empty_example(data[0].meanings[i].definitions[0].example)}</span></p> 
                 </div>
         
         `
